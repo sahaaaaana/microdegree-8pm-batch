ae8f5d6afeef91a63dbd796f2b1fb68da2475387
ManojKRISHNAPPA <107530108+ManojKRISHNAPPA@users.noreply.github.com>
2023-02-14 00:54:31 +0530

    Update main.tf
    Signed-off-by: ManojKRISHNAPPA <107530108+ManojKRISHNAPPA@users.noreply.github.com>

--- terrform/main.tf
+++ terrform/main.tf
@@ -9,27 +9,28 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  
+  access_key = "xxxxx"
+  secret_key = "xxxx"
 }
 
 
 resource "aws_instance" "myinstance01" {
-  ami           = "ami-061ac2e015473fbe2" 
-  instance_type = "t2.small"
+  ami           = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
   count = "1"
-  
 
-  
+
+
   tags = {
     Name = "Server_1-W1"
   }
 
 }
 resource "aws_instance" "myinstance02" {
-  ami           = "ami-061ac2e015473fbe2" 
-  instance_type = "t2.small"
+  ami           = "ami-007868005aea67c54"
+  instance_type = "t2.micro"
   count = "1"
-  
+
   tags = {
     Name = "Server_2-W1"
   }
